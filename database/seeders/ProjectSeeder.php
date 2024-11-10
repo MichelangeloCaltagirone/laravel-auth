f8f15fd73af8e97ce8b26c01cfea1d5f37eb838c
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-10 11:41:38 +0100

    Rollback the auth db and add Faker generator to the ProjectSeeder for more data.

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,39 +5,47 @@ namespace Database\Seeders;
 use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $projects = [
-            [
-            "name" => "Liuteria",
-            "author" => "Andrea",
-            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime magnam, aperiam iste consectetur nesciunt illo quo cupiditate ducimus adipisci molestias architecto, omnis, vitae quibusdam quasi. Beatae autem unde excepturi"
-            ],
-            [
-            "name" => "Fotografia",
-            "author" => "Barbara",
-            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime magnam, aperiam iste consectetur nesciunt illo quo cupiditate ducimus adipisci molestias architecto, omnis, vitae quibusdam quasi. Beatae autem unde excepturi"
-            ],
-            [
-            "name" => "Illustrazione",
-            "author" => "Pietro",
-            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime magnam, aperiam iste consectetur nesciunt illo quo cupiditate ducimus adipisci molestias architecto, omnis, vitae quibusdam quasi. Beatae autem unde excepturi"
-            ],
-        ];
+        // $projects = [
+        //     [
+        //     "name" => "Liuteria",
+        //     "author" => "Andrea",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime magnam, aperiam iste consectetur nesciunt illo quo cupiditate ducimus adipisci molestias architecto, omnis, vitae quibusdam quasi. Beatae autem unde excepturi"
+        //     ],
+        //     [
+        //     "name" => "Fotografia",
+        //     "author" => "Barbara",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime magnam, aperiam iste consectetur nesciunt illo quo cupiditate ducimus adipisci molestias architecto, omnis, vitae quibusdam quasi. Beatae autem unde excepturi"
+        //     ],
+        //     [
+        //     "name" => "Illustrazione",
+        //     "author" => "Pietro",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime magnam, aperiam iste consectetur nesciunt illo quo cupiditate ducimus adipisci molestias architecto, omnis, vitae quibusdam quasi. Beatae autem unde excepturi"
+        //     ],
+        // ];
 
-        foreach($projects as $project) {
+        for($i = 0; $i < 100; $i++) {
             $newProject = new Project();
-            $newProject->name = $project["name"];
-            $newProject->author = $project["author"];
-            $newProject->description = $project["description"];
+            $newProject->name = $faker->firstName();
+            $newProject->author = $faker->firstName() .' '. $faker->unique()->lastName();
+            $newProject->description = $faker->realTextBetween(150, 450);
             $newProject->save();
         }
+        // foreach($projects as $project) {
+        //     $newProject = new Project();
+        //     $newProject->name = $project["name"];
+        //     $newProject->author = $project["author"];
+        //     $newProject->description = $project["description"];
+        //     $newProject->save();
+        // }
     }
 
 }
